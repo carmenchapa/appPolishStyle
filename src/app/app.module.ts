9d3c406544a4bdd2e7ebad1ba06adba3f75b0777
Carmen Chapa <cmchapa28@gmail.com>
2017-10-30 13:34:55 +0000

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
+import { ReservaPage } from '../pages/reserva/reserva';
+import { ItemDetailsPage } from '../pages/item-details/item-details';
+import { ListPage } from '../pages/list/list';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HelloIonicPage,
+    ReservaPage,
+    ItemDetailsPage,
+    ListPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HelloIonicPage,
+    ReservaPage,
+    ItemDetailsPage,
+    ListPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/reserva/reserva.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+// import { DateTime } from 'ionic-angular';
+
+@Component({
+  selector: 'page-reserva',
+  templateUrl: 'reserva.html'
+})
+export class ReservaPage {
+	public event = {
+	    // month: '1990-02-19',
+	    timeStarts: '2018-01-19',
+	    timeEnds: '2017-01-24'
+	}
+  constructor() {
+
+  }
+}
