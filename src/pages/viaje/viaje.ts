33246f6b8eac1a019a52f1303604f1de5f47d747
Carmen Chapa <cmchapa28@gmail.com>
2017-10-30 17:24:54 +0000

    create slides

--- src/pages/viaje/viaje.ts
+++ src/pages/viaje/viaje.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { GetReadyPage } from '../pages/get-ready/get-ready';
 
 @Component({
   selector: 'viaje',
@@ -7,16 +8,20 @@ import { Component } from '@angular/core';
 export class ViajePage {
 	slides = [
     {
+    	// component: GetReadyPage,
       title: "Welcome to the Docs!",
       description: "The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.",
       image: "assets/imgs/ica-slidebox-img-1.png",
     },
     {
+    	// component: GetReadyPage,
       title: "What is Ionic?",
       description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
       image: "assets/imgs/ica-slidebox-img-2.png",
     },
     {
+    	// component: GetReadyPage,
+    	
       title: "What is Ionic Cloud?",
       description: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
       image: "assets/imgs/ica-slidebox-img-3.png",
